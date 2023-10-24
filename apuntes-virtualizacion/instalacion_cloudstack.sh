67b78207e2afe1a51b146f5575c82b860f4aad5b
gonzalonata06 <78128431+gonzalonata06@users.noreply.github.com>
2023-10-24 12:35:08 -0600

    Update instalacion_cloudstack.sh

--- apuntes-virtualizacion/instalacion_cloudstack.sh
+++ apuntes-virtualizacion/instalacion_cloudstack.sh
@@ -121,15 +121,15 @@ echo BRIDGE=cloudbr0 >> $r_red2
 
 #Creacion del archivo de configuracion para la interfaz que se conecta a internet
 
-subip_int=$(echo $(ip r l | grep -o "def.*dev.*[0-9].proto" | cut -f3 -d" " | cut -f1 -d.))
+subip_int= echo $(ip r l | grep -o "def.*dev.*[0-9].proto" | cut -f3 -d" " | cut -f1 -d.)
 
 for  ((i=2;i<3;i++)){
 	subip_int="$subip_int.$(echo $(ip r l | grep -o "def.*dev.*[0-9].proto" | cut -f3 -d" " | cut -f$i -d.))"
 }
 
 
-interfaz_internet=$(echo $(ip r l | grep -o "def.*dev.*[0-9].proto" | cut -f5 -d" "))
-ip_int=$(echo $(ip a l | grep -o "inet.*$subip_int.[0-9]\{1,3\}/" | tr -d "/" | cut -f2 -d" "))
+interfaz_internet= echo $(ip r l | grep -o "def.*dev.*[0-9].proto" | cut -f5 -d" ")
+ip_int= echo $(ip a l | grep -o "inet.*$subip_int.[0-9]\{1,3\}/" | tr -d "/" | cut -f2 -d" ")
 gateway=""
 
 gateway="$(echo $ip_int | cut -f1 -d.)"
